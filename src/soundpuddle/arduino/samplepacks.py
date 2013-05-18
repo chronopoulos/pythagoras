2937f7461f1446f149b5ad0c7455e0aea5385e8c
Chris Chronopoulos <chrono@t400s.(none)>
2013-05-18 16:12:28 -0600

    cleaned up python script for soundflower

---
+++ src/soundpuddle/arduino/samplepacks.py
@@ -0,0 +1,92 @@
+"""
+These are just lists of filenames.
+"""
+
+tr606 = [
+'../../../samples/drums/tr606/kick.wav',
+'../../../samples/drums/tr606/snare.wav',
+'../../../samples/drums/tr606/closedhat.wav',
+'../../../samples/drums/tr606/openhat.wav',
+'../../../samples/drums/tr606/cymbal.wav',
+'../../../samples/drums/tr606/tom.wav'            
+]
+
+tr707 = [
+'../../../samples/drums/tr707/kick.wav',
+'../../../samples/drums/tr707/snare.wav',
+'../../../samples/drums/tr707/closedhat.wav',
+'../../../samples/drums/tr707/openhat.wav',
+'../../../samples/drums/tr707/clap.wav',
+'../../../samples/drums/tr707/tambourine.wav'            
+]
+
+tr808 = [
+'../../../samples/drums/tr808/kick.wav',
+'../../../samples/drums/tr808/snare.wav',
+'../../../samples/drums/tr808/closedhat.wav',
+'../../../samples/drums/tr808/openhat.wav',
+'../../../samples/drums/tr808/clap.wav',
+'../../../samples/drums/tr808/cowbell.wav'            
+]
+
+tr909 = [
+'../../../samples/drums/tr909/kick.wav',
+'../../../samples/drums/tr909/snare.wav',
+'../../../samples/drums/tr909/closedhat.wav',
+'../../../samples/drums/tr909/openhat.wav',
+'../../../samples/drums/tr909/clap.wav',
+'../../../samples/drums/tr909/ride.wav'            
+]
+
+linndrum = [
+'../../../samples/drums/linndrum/kick.wav',
+'../../../samples/drums/linndrum/snare.wav',
+'../../../samples/drums/linndrum/closedhat.wav',
+'../../../samples/drums/linndrum/openhat.wav',
+'../../../samples/drums/linndrum/clap.wav',
+'../../../samples/drums/linndrum/cowbell.wav'            
+]
+
+dundunba = [
+'../../../samples/drums/dundunba/dundunba.wav',
+'../../../samples/drums/dundunba/kenkeni.wav',
+'../../../samples/drums/dundunba/sangban.wav',
+'../../../samples/drums/dundunba/sangban-mute.wav'           
+]
+
+rx21Latin = [
+'../../../samples/drums/rx21Latin/conga.wav',
+'../../../samples/drums/rx21Latin/bongo.wav',
+'../../../samples/drums/rx21Latin/timbale.wav',
+'../../../samples/drums/rx21Latin/cuica.wav',
+'../../../samples/drums/rx21Latin/agogo.wav',
+'../../../samples/drums/rx21Latin/cowbell.wav'            
+]
+
+koto = [
+'../../../samples/cmajorpentatonic/koto/c3.wav',
+'../../../samples/cmajorpentatonic/koto/d3.wav',
+'../../../samples/cmajorpentatonic/koto/e3.wav',
+'../../../samples/cmajorpentatonic/koto/g3.wav',
+'../../../samples/cmajorpentatonic/koto/a3.wav',
+'../../../samples/cmajorpentatonic/koto/c4.wav'            
+]
+
+rhodes = [
+'../../../samples/cmajorpentatonic/rhodes/c2.aif',
+'../../../samples/cmajorpentatonic/rhodes/d2.aif',
+'../../../samples/cmajorpentatonic/rhodes/e2.aif',
+'../../../samples/cmajorpentatonic/rhodes/g2.aif',
+'../../../samples/cmajorpentatonic/rhodes/a2.aif',
+'../../../samples/cmajorpentatonic/rhodes/c3.aif'            
+]
+
+chimes = [
+'../../../samples/cmajorpentatonic/chimes/c5.wav',
+'../../../samples/cmajorpentatonic/chimes/d5.wav',
+'../../../samples/cmajorpentatonic/chimes/e5.wav',
+'../../../samples/cmajorpentatonic/chimes/g5.wav',
+'../../../samples/cmajorpentatonic/chimes/a5.wav',
+'../../../samples/cmajorpentatonic/chimes/c6.wav'            
+]
+
